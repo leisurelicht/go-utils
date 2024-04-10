fe5089b9f1dbfb8787caf1a82750bbdc64fe86af
暮晨 <leisurelicht@gmail.com>
2024-04-10 16:59:24 +0800

    feat: 🎸 add some function

---
+++ gen/str.go
@@ -0,0 +1,54 @@
+package gen
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomStrings(length, strLength int, includeList, avoidList []string) []string {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	result := make([]string, length)
+
+	for i := 0; i < length; i++ {
+		var randomStr string
+		for {
+			randomStr = ""
+			for j := 0; j < strLength; j++ {
+				randomStr += string(charset[rand.Intn(len(charset))])
+			}
+
+			include := true
+			for _, insluceStr := range includeList {
+				if randomStr == insluceStr {
+					include = false
+					break
+				}
+			}
+
+			if !include {
+				continue
+			}
+
+			avoid := false
+			for _, avoidStr := range avoidList {
+				if randomStr == avoidStr {
+					avoid = true
+					break
+				}
+			}
+
+			if avoid {
+				continue
+			}
+
+			break
+		}
+
+		result[i] = randomStr
+	}
+
+	return result
+}
---
+++ in/in_slice.go
@@ -0,0 +1,104 @@
+package check
+
+func InSlice(val any, s any) bool {
+	switch s.(type) {
+	case []string:
+		return InStringSlice(val.(string), s.([]string))
+	case []int:
+		return InIntSlice(val.(int), s.([]int))
+	case []int64:
+		return InInt64Slice(val.(int64), s.([]int64))
+	case []float64:
+		return InFloat64Slice(val.(float64), s.([]float64))
+	}
+
+	return false
+}
+
+func InStringSlice(val string, s []string) bool {
+    if len(s) == 0 {
+        return false
+    }else if len(s) < 100 {
+		for _, item := range s {
+			if item == val {
+				return true
+			}
+		}
+	} else {
+		i, j := 0, len(s)-1
+		for i <= j {
+			if s[i] == val || s[j] == val {
+				return true
+			}
+			i++
+			j--
+		}
+	}
+	return false
+}
+
+func InIntSlice(val int, s []int) bool {
+    if len(s) == 0 {
+        return false
+    }else if len(s) < 100 {
+		for _, item := range s {
+			if item == val {
+				return true
+			}
+		}
+	} else {
+		i, j := 0, len(s)-1
+		for i <= j {
+			if s[i] == val || s[j] == val {
+				return true
+			}
+			i++
+			j--
+		}
+	}
+	return false
+}
+
+func InInt64Slice(val int64, s []int64) bool {
+    if len(s) == 0 {
+        return false
+    }else if len(s) < 100 {
+		for _, item := range s {
+			if item == val {
+				return true
+			}
+		}
+	} else {
+		i, j := 0, len(s)-1
+		for i <= j {
+			if s[i] == val || s[j] == val {
+				return true
+			}
+			i++
+			j--
+		}
+	}
+	return false
+}
+
+func InFloat64Slice(val float64, s []float64) bool {
+        if len(s) == 0 {
+        return false
+    }else if len(s) < 100 {
+		for _, item := range s {
+			if item == val {
+				return true
+			}
+		}
+	} else {
+		i, j := 0, len(s)-1
+		for i <= j {
+			if s[i] == val || s[j] == val {
+				return true
+			}
+			i++
+			j--
+		}
+	}
+	return false
+}
